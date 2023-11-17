54780200c553cb836edd35c2eb9893686e1ca941
iranilsj <irani.fpo@gmail.com>
2023-11-17 17:00:50 -0300

    Alterando e adicionando objeto

---
+++ alterando.js
@@ -0,0 +1,19 @@
+const pessoa = {
+    nome: "Luma",
+    profissão: "Engenheira"
+}
+console.log();
+console.log("1)", pessoa.nome);
+console.log();
+
+console.log("2)", pessoa.telefone);
+console.log();
+
+pessoa.telefone = "(11) 1122-3344";
+console.log("3)", pessoa.telefone);
+console.log();
+
+// Adicionando informação
+
+pessoa.nome = "Luma Silva"
+console.log(pessoa);
