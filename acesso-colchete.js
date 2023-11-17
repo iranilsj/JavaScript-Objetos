0bb421335e9fa8ef8624307f2a72f7cbeeb38b7b
iranilsj <irani.fpo@gmail.com>
2023-11-17 11:29:29 -0300

    Outra forma de acesssa objeto

---
+++ acesso-colchete.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome: "André",
+    idade: 32,
+    cpf: "123.xxx.xxx-xx",
+    email: "xxx@xxxxx.com",
+}
+console.log()
+console.log(`O nome do(a) cliente é: ${cliente["nome"]}! e esse(a) cliente tem: ${cliente["idade"]}.`)
+console.log(`Os 3 primeiros digitos do CPf são: ${cliente["cpf"].substring(0, 3)}`)
+console.log()
+
+const chaves = ["nome", "idade", "cpf", "email"]
+
+chaves.forEach((chave) => {
+    console.log(`A chave ${chave} tem valor ${cliente[chave]}`)
+})
+console.log()
