d0d21b1d2136708eff9abbd0e1dfb416c962d5f1
iranilsj <irani.fpo@gmail.com>
2023-11-17 10:19:05 -0300

    test 1

---
+++ objeto.js
@@ -0,0 +1,17 @@
+const listaCPFs = [1111111, 45454545, 656565656];
+
+const informacoesPessoa = ['nome', 'Jose', 'Idade', 32, "CPF", '545.444.545-20'];
+
+console.log()
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    //Propriedades  -> Chave nome, Valor "José"
+    nome: "José",
+    idade: 32,
+    CPF: '545.444.545-20'
+
+}
+console.log()
+console.log(objetoPessoa)
+console.log()
